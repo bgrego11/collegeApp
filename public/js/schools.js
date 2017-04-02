27113458140b111e77ed6cdbc4642957dfb68b4d
makhno <makhno@makhnos-MacBook-Pro.local>
2017-04-02 00:13:27 -0400

    touch ups

--- public/js/schools.js
+++ public/js/schools.js
@@ -8,59 +8,13 @@ $.get("/myCollege/"+sessionStorage.id, function(data) {
   var myData = data;
   console.log(myData);
 
-
+//add colleges to table
   for (i in myData) {
     col = myData[i]
     table = $('#schoolTable')
     table.append($('<tr><td>'+col.College+'</td><td>'+col.City+'</td><td>'+col.State+'</td><td>'+(col.Admission*100).toFixed(2)+'</td><td>'+col.Tuition_In+'</td><td>'+col.Tuition_Out+'</td></tr>'))
   }
-  // //create 5 arrays for each data object:
-  // 	newArray0 = myData[0];
-  // 	newArray1 = myData[1];
-  // 	newArray2 = myData[2];
-  // 	newArray3 = myData[3];
-  // 	newArray4 = myData[4];
   
-  // //Displaying Each Array Data
-  // Object.getOwnPropertyNames(newArray0).forEach(
-  // 		function (val, idx, array) {
-  // 			newDiv = $("<div>");
-  // 			newDiv.addClass("well");
-  // 			newDiv.append(val + ' : ' + newArray0[val])
-  // 			$('#schoolOne').append(newDiv);
-  // 		});
-
-  //  Object.getOwnPropertyNames(newArray1).forEach(
-  // 		function (val, idx, array) {
-  // 			newDiv = $("<div>");
-  // 			newDiv.addClass("well");
-  // 			newDiv.append(val + ' : ' + newArray1[val])
-  // 			$('#school2').append(newDiv);
-  // 		});
-
-  //   Object.getOwnPropertyNames(newArray2).forEach(
-  // 		function (val, idx, array) {
-  // 			newDiv = $("<div>");
-  // 			newDiv.addClass("well");
-  // 			newDiv.append(val + ' : ' + newArray2[val])
-  // 			$('#school3').append(newDiv);
-  // 		});
-
-  //    Object.getOwnPropertyNames(newArray3).forEach(
-  // 		function (val, idx, array) {
-  // 			newDiv = $("<div>");
-  // 			newDiv.addClass("well");
-  // 			newDiv.append(val + ' : ' + newArray3[val])
-  // 			$('#school4').append(newDiv);
-  // 		});
-
-  //     Object.getOwnPropertyNames(newArray4).forEach(
-  // 		function (val, idx, array) {
-  // 			newDiv = $("<div>");
-  // 			newDiv.addClass("well");
-  // 			newDiv.append(val + ' : ' + newArray4[val])
-  // 			$('#school5').append(newDiv);
-  		
 
   });
   });
